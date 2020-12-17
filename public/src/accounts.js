3b681fe8a05a32b127134ad722e72d37ef77cde2
Alex Stout <astoutj@gmail.com>
2020-12-17 11:29:36 -0700

    made some comments and formatted the code to improve readbility

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -6,27 +6,31 @@ function findAccountById(accounts, id) {
   }
 }
 
-var left = -1;
-var right = 1;
+// should use let instead of var to declare variables
+let left = -1;
+let right = 1;
 function sortAccountsByLastName(accounts) {
-return accounts.sort((a,b) => a.name.last<b.name.last ? left : right );
+  return accounts.sort(
+    (a,b) => a.name.last<b.name.last ? left : right
+  );
 }
 
 function numberOfBorrows(account, books) { 
   let total = 0;
-  for (let index in books){
+  for (let index in books) {
     const borrowed = books[index].borrows.filter(book=> book.id === account.id).length;
     total += borrowed;
   }
   return total;
 }
 
-
 function booksInPossession(account, books, authors) {
   let arr = [];
-  for (let index in books){
-    let booksOut = books[index].borrows.some(book=> book.id === account.id && book.returned === false);
-      if (booksOut){
+  for (let index in books) {
+    let booksOut = books[index]
+      .borrows
+      .some(book => (book.id === account.id && book.returned === false));
+      if (booksOut) {
         let bookIndex = books[index];
         bookIndex.author = authors.find(authorFound=> authorFound.id === bookIndex.authorId);
         arr.push(bookIndex);
@@ -34,7 +38,6 @@ function booksInPossession(account, books, authors) {
   }
   return arr;
 }
-
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,25 +1,25 @@
 function findAuthorById(authors, id) {
-    for(let i=0;i<authors.length;i++){
-    if(authors[i].id === id){
+  for (let i = 0; i < authors.length; i++) {
+    if (authors[i].id === id) {
       return authors[i];
     }
   }
 }
 
 function findBookById(books, id) {
-  for(let i=0;i<books.length;i++){
-    const {id: bookID} = books[i];
+  for (let i = 0; i < books.length; i++) {
+    const {id: bookID} = books[i]; // nice destructuring with renaming
     if (bookID === id) {
-      return books[i]
+      return books[i];
     }
   }
 }
 
 function partitionBooksByBorrowedStatus(books) {
   let borrowedBooks = books.filter((book) => book.borrows[0].returned === false);
-  let returnedBooks = books.filter(book=>{
-    return book.borrows[0].returned==true;
-  })
+  let returnedBooks = books.filter(book => {
+    return book.borrows[0].returned == true;
+  });
   return [borrowedBooks, returnedBooks];
 }
 
@@ -36,18 +36,17 @@ function partitionBooksByBorrowedStatus(books) {
 // }
 function getBorrowersForBook(book, accounts){
   let borrowers = [];
-  accounts.forEach(account=>{
-  book.borrows.forEach(transaction=>{
-    if(transaction.id === account.id){
-      let accountObj = {...account};
-      accountObj.returned = transaction.returned;
-      borrowers.push(accountObj);
-    }
-  })
-})
-  return borrowers.slice(0,10);
+  accounts.forEach(account => {
+    book.borrows.forEach(transaction => {
+      if (transaction.id === account.id) {
+        let accountObj = {...account}; // nice object spreading
+        accountObj.returned = transaction.returned;
+        borrowers.push(accountObj);
+      }
+    });
+  });
+  return borrowers.slice(0, 10);
 }
-
 
 module.exports = {
   findAuthorById,
